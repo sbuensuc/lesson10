a13cc2df39ce39a064eb3f660204e7320ac363c6
Sam <Sam@DESKTOP-O0S9A91>
2016-11-28 14:24:19 -0500

    Add project files.

---
+++ lesson10/Controllers/GenresController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lesson10.Models;
+
+namespace lesson10.Controllers
+{
+    public class GenresController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Genres
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        // GET: Genres/Details/5
+        public ActionResult Details(int? id = 1)
+        {
+
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+
+
+    }
+}
---
+++ lesson10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lesson10.Startup))]
+namespace lesson10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
